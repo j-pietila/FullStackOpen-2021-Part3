aeb882d240b101a442b4284c3f1d6cf6e7cb3985
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2021-11-09 13:25:30 +0200

    Fixed indentation levels for mongo.js this time hopefully for real

--- mongo.js
+++ mongo.js
@@ -5,10 +5,10 @@ const mongoose = require("mongoose")
 // node mongo.js yourpassword "Arto Vihavainen" 040-1234556: Adds new person to phonebook
 
 if (process.argv.length < 3 ||
-	 (process.argv.length > 3 && process.argv.length < 5) ||
-	 (process.argv.length > 5)) {
-		console.log("Wrong number of arguments. Check code file for proper program usage.")
-		process.exit(1)
+   (process.argv.length > 3 && process.argv.length < 5) ||
+   (process.argv.length > 5)) {
+     console.log("Wrong number of arguments. Check code file for proper program usage.")
+     process.exit(1)
 }
 
 const password = process.argv[2]
@@ -17,46 +17,45 @@ const url = `mongodb+srv://db_access:${password}@testcluster.5my5d.mongodb.net/p
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model("Person", personSchema)
 
 const getPersons = () => {
-	console.log("Phonebook:")
+  console.log("Phonebook:")
 
-	Person
-		.find({})
-		.then(result => {
-			result.forEach(person => {
-			console.log(`${person.name} ${person.number}`)
-		})
-		mongoose.connection.close()
-	})
+  Person
+    .find({})
+    .then(result => {
+      result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 }
 
 const addPerson = () => {
-	const newName = process.argv[3]
-	const newNumber = process.argv[4]
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
 	
-	const person = new Person({
-		name: newName,
-		number: newNumber,
-	})
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  })
 
-	person
-		.save()
-		.then(response => {
-			console.log(`Added ${newName} number ${newNumber} to phonebook`)
-			mongoose.connection.close()
-		})
+  person
+    .save()
+    .then(response => {
+      console.log(`Added ${newName} number ${newNumber} to phonebook`)
+      mongoose.connection.close()
+    })
 }
 
 if (process.argv.length === 3) { // Only password as a parameter
-	getPersons()
+  getPersons()
 }
 else if (process.argv.length === 5) { // Password, name and number as parameters
-	addPerson()
+  addPerson()
 }
-
